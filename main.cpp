53e9e705c22db62c1e0e837b5fb1fbaebd9a84c8
boredkevin <kevin@boredkevin.com>
2025-07-03 22:55:39 +0700

    Use codeblocks as codebase

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <fstream>
 
-int main(){
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+using namespace std;
+
+ifstream fin("data.in");
+ofstream fout("data.out");
+
+int read(double a, double b)
+{
+    double c;
+    c = a + b;
+    return c;
+}
+
+int main()
+{
+    double a, b;
+        fin>>a;
+        fout<<"Goth Mommies = "<< a;
+        fin>>b;
+        fout<<"\nGoth Daddies = "<< b;
+    fout<< "\nTotal number of Goths = " << read(a, b);
+    return 0;
 }
